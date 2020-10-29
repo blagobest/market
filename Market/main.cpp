399051deef4385f46484b906b2999af5efb6d719
Blagovest Gospodinov <simplyblago@gmail.com>
2020-10-29 23:35:29 +0200

    Initial Commit

---
+++ Market/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Market
+//
+//  Created by Blagovest Gospodinov on 29.10.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
